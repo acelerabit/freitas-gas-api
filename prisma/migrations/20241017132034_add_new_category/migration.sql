6548f51a5c94f580dc6c37404266449d334bb8f2
gaba <gabrielferrsantos201@gmail.com>
2024-10-17 10:58:06 -0300

    fix: migrations

--- prisma/migrations/20241017132034_add_new_category/migration.sql
+++ prisma/migrations/20241017132034_add_new_category/migration.sql
@@ -1,16 +1,1 @@
--- This is an empty migration.
-
-
-
--- This is an empty migration.
-
-DO $$ 
-BEGIN
-    -- Check if the value 'WITHDRAW' exists in the enum 'TransactionCategory'
-    IF NOT EXISTS (SELECT 1 FROM pg_enum 
-                   WHERE enumlabel = 'WITHDRAW' 
-                   AND enumtypid = (SELECT oid FROM pg_type WHERE typname = 'TransactionCategory')) THEN
-        -- Add the value to the enum if it doesn't exist
-        ALTER TYPE "TransactionCategory" ADD VALUE 'WITHDRAW';
-    END IF;
-END $$;
+ALTER TYPE "TransactionCategory" ADD VALUE 'WITHDRAW';
